91aa9853b02680c0f497a20f426ed63ab60bdea6
Daniel Galvão <daniel.galvao@justdigital.com.br>
2016-08-09 09:51:50 -0300

    fix identing

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,26 +4,27 @@
 
 /*globals jQuery, document */
 (function ($) {
-    "use strict";
+  "use strict";
 
-    $(document).ready(function(){
+  $(document).ready(function(){
 
-        $(".post-content").fitVids();
-        
-        // Calculates Reading Time
-        $('.post-content').readingTime({
-            readingTimeTarget: '.post-reading-time',
-            wordCountTarget: '.post-word-count',
-        });
-        
-        // Creates Captions from Alt tags
-        $(".post-content img").each(function() {
-            // Let's put a caption if there is one
-            if($(this).attr("alt") && !$(this).hasClass("emoji"))
-              $(this).wrap('<figure class="image"></figure>')
-              .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
-        });
-        
+    $(".post-content").fitVids();
+
+    // Calculates Reading Time
+    $('.post-content').readingTime({
+      readingTimeTarget: '.post-reading-time',
+      wordCountTarget: '.post-word-count',
     });
+
+    // Creates Captions from Alt tags
+    $(".post-content img").each(function() {
+      // Let's put a caption if there is one
+      if($(this).attr("alt") && !$(this).hasClass("emoji")) {
+        $(this).wrap('<figure class="image"></figure>')
+          .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
+      }
+    });
+
+  });
 
 }(jQuery));
